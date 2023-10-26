2789955cab387a3fb8794def67d77d0a46f6ae44
Garrett Nelson <gjnels@proton.me>
2023-10-26 12:22:05 -0400

    Check for win condition

--- src/lib/board.ts
+++ src/lib/board.ts
@@ -6,8 +6,8 @@ export enum Cell {
   YELLOW
 }
 
-const BOARD_WIDTH = 7
-const BOARD_HEIGHT = 6
+export const BOARD_WIDTH = 7
+export const BOARD_HEIGHT = 6
 
 const boardStore = writable<Cell[][]>([])
 
@@ -44,9 +44,110 @@ function placeMarker(col: number, marker: Cell.RED | Cell.YELLOW) {
   return true
 }
 
+// check if there are no empty cells on the board
+function isFull() {
+  return get(boardStore).every((col) => col.every((cell) => cell !== Cell.EMPTY))
+}
+
+// determine if there are 4 pieces in a row of the same color horizontally
+function checkForHorizontalWin() {
+  for (let col = 0; col <= BOARD_WIDTH - 4; col++) {
+    for (let cell = 0; cell < BOARD_HEIGHT; cell++) {
+      if (get(board)[col][cell] === Cell.EMPTY) continue
+      if (
+        get(board)[col][cell] === get(board)[col + 1][cell] &&
+        get(board)[col][cell] === get(board)[col + 2][cell] &&
+        get(board)[col][cell] === get(board)[col + 3][cell]
+      ) {
+        return {
+          winner: get(board)[col][cell],
+          cells: [
+            [col, cell],
+            [col + 1, cell],
+            [col + 2, cell],
+            [col + 3, cell]
+          ]
+        }
+      }
+    }
+  }
+  return false
+}
+
+// determine if there are 4 pieces in a row of the same color vertically
+function checkForVerticalWin() {
+  for (let col = 0; col < BOARD_WIDTH; col++) {
+    for (let cell = 0; cell <= BOARD_HEIGHT - 4; cell++) {
+      if (get(board)[col][cell] === Cell.EMPTY) continue
+      if (
+        get(board)[col][cell] === get(board)[col][cell + 1] &&
+        get(board)[col][cell] === get(board)[col][cell + 2] &&
+        get(board)[col][cell] === get(board)[col][cell + 3]
+      ) {
+        return {
+          winner: get(board)[col][cell],
+          cells: [
+            [col, cell],
+            [col, cell + 1],
+            [col, cell + 2],
+            [col, cell + 3]
+          ]
+        }
+      }
+    }
+  }
+  return false
+}
+
+function checkForDiagonalWin() {
+  for (let col = 0; col <= BOARD_WIDTH - 4; col++) {
+    for (let cell = 0; cell <= BOARD_HEIGHT - 4; cell++) {
+      if (get(board)[col][cell] === Cell.EMPTY) continue
+      if (
+        get(board)[col][cell] === get(board)[col + 1][cell + 1] &&
+        get(board)[col][cell] === get(board)[col + 2][cell + 2] &&
+        get(board)[col][cell] === get(board)[col + 3][cell + 3]
+      )
+        return {
+          winner: get(board)[col][cell],
+          cells: [
+            [col, cell],
+            [col + 1, cell + 1],
+            [col + 2, cell + 2],
+            [col + 3, cell + 3]
+          ]
+        }
+    }
+    for (let cell = 3; cell < BOARD_HEIGHT; cell++) {
+      if (get(board)[col][cell] === Cell.EMPTY) continue
+      if (
+        get(board)[col][cell] === get(board)[col + 1][cell - 1] &&
+        get(board)[col][cell] === get(board)[col + 2][cell - 2] &&
+        get(board)[col][cell] === get(board)[col + 3][cell - 3]
+      )
+        return {
+          winner: get(board)[col][cell],
+          cells: [
+            [col, cell],
+            [col + 1, cell - 1],
+            [col + 2, cell - 2],
+            [col + 3, cell - 3]
+          ]
+        }
+    }
+  }
+  return false
+}
+
+function checkForWin() {
+  return checkForVerticalWin() || checkForHorizontalWin() || checkForDiagonalWin()
+}
+
 export const board = {
   subscribe: boardStore.subscribe,
   createNewBoard,
   placeMarker,
-  isValidColumn
+  isValidColumn,
+  isFull,
+  checkForWin
 }
--- src/lib/game.ts
+++ src/lib/game.ts
@@ -4,6 +4,13 @@ import { Cell, board } from './board'
 type Player = Cell.RED | Cell.YELLOW
 
 const turnStore = writable<Player>()
+const winStore = writable<
+  | false
+  | {
+      winner: Cell
+      cells: number[][]
+    }
+>(false)
 
 function switchTurn() {
   turnStore.update((cur) => (cur === Cell.RED ? Cell.YELLOW : Cell.RED))
@@ -12,18 +19,28 @@ function switchTurn() {
 // try to place a marker for the current player at given position
 // switch turns if marker is placed usccessfully
 function takeTurn(col: number) {
-  if (board.placeMarker(col, get(turnStore))) switchTurn()
+  if (board.checkForWin() || !board.placeMarker(col, get(turnStore))) return
+  const win = board.checkForWin()
+  if (win) {
+    winStore.set(win)
+  } else {
+    switchTurn()
+  }
 }
 
 // create a new game board and randomly choose a starting player
 function newGame() {
   board.createNewBoard()
   turnStore.set(Math.random() > 0.5 ? Cell.RED : Cell.YELLOW)
+  winStore.set(false)
 }
 
 export const game = {
   turn: {
     subscribe: turnStore.subscribe
+  },
+  winner: {
+    subscribe: winStore.subscribe
   },
   switchTurn,
   newGame,
