d99aa395a00e5f39d4ac5e293e5f5d05885378df
Garrett Nelson <gjnels@proton.me>
2023-10-26 13:02:23 -0400

    Create visual board

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,10 @@
   import '../app.css'
 </script>
 
-<slot />
+<header class="mb-4 px-4 py-2">
+  <h1 class="font-bold text-2xl">Connect 4 Predictions</h1>
+</header>
+
+<main class="px-4 py-2">
+  <slot />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,42 @@
-<h1>Connect 4 Predictions</h1>
+<script lang="ts">
+  import { BOARD_HEIGHT, BOARD_WIDTH, Cell, board, game } from '$lib'
+
+  const { winner } = game
+  game.newGame()
+</script>
+
+{#if $board.length}
+  <div
+    class="flex max-w-2xl mx-auto rounded-md overflow-hidden"
+    style="aspect-ratio: {BOARD_WIDTH} / {BOARD_HEIGHT};"
+  >
+    {#each $board as col, colIndex (colIndex)}
+      <div
+        class="flex grow flex-col-reverse bg-blue-700 {!$winner && board.isValidColumn(colIndex)
+          ? 'hover:brightness-110'
+          : ''}"
+      >
+        {#each col as cell, rowIndex (rowIndex)}
+          {@const winningCell =
+            $winner && !!$winner.cells.find((cell) => cell[0] === colIndex && cell[1] === rowIndex)}
+          <button
+            on:click={() => game.takeTurn(colIndex)}
+            class="flex justify-center items-center grow {!$winner && board.isValidColumn(colIndex)
+              ? 'cursor-pointer'
+              : 'cursor-not-allowed'}"
+          >
+            <span
+              class="w-3/4 aspect-square rounded-full ring-4 {cell === Cell.RED
+                ? 'bg-red-500'
+                : cell === Cell.YELLOW
+                ? 'bg-yellow-400'
+                : 'bg-gray-100 dark:bg-gray-900'} {winningCell
+                ? 'dark:ring-gray-100 ring-gray-900 brightness-110'
+                : 'ring-transparent'}"
+            />
+          </button>
+        {/each}
+      </div>
+    {/each}
+  </div>
+{/if}
