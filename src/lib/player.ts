b820c688ade538a024e4e2798dcae4b85256cddb
Garrett Nelson <gjnels@proton.me>
2023-11-30 11:24:33 -0500

    User can choose player type and ai difficulty from main menu

--- src/lib/player.ts
+++ src/lib/player.ts
@@ -4,15 +4,26 @@ export const HUMAN = 0
 export const AI = 1
 
 export type PieceColor = 'red' | 'yellow'
+export type Difficulty = 'easy' | 'medium' | 'hard'
 
 export class Player {
   public piece: Piece
   public color: PieceColor
   public ai: boolean
+  public difficulty: Difficulty
 
-  constructor(piece: Piece, color: PieceColor, ai = false) {
+  constructor(piece: Piece, color: PieceColor, ai: boolean, difficulty?: Difficulty) {
     this.piece = piece
     this.color = color
     this.ai = ai
+    this.difficulty = difficulty || 'medium'
+  }
+
+  setAi(ai: boolean) {
+    this.ai = ai
+  }
+
+  setDifficulty(difficulty: Difficulty) {
+    this.difficulty = difficulty
   }
 }
